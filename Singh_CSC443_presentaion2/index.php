c1af2dd0a5abba967f65e244432de89deade4b06
Jass-Singh-20 <81336464+Jass-Singh-20@users.noreply.github.com>
2022-10-26 15:06:32 -0400

    Add files via upload

---
+++ Singh_CSC443_presentaion2/index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        <link rel="stylesheet" href="css/webslides.css">
+        <title>Jasdip Singh</title>
+
+    </head>
+    <body>
+        <main role="main"></main>
+        <article id="webslides" class="horizontal">
+             <!-- Slide 1 -->
+            <section>
+                <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+           <center>     <div class="wrap">
+                    
+                    <h1><p style="color:white;">Jasdip Singh</p>                       
+                    </h1>
+                    <br><br>
+                    <h2 ><p style="color:white;">Presentation 2</p></h2>
+                    <br><br>
+                    <h2 ><p style="color:white;">Southern Connecticut State University </p></h2>
+                    <img src="img/mypic.jpg" alt="" style="width:300px;height:250px;">
+                </div> </center>
+                </section>
+                <!--slide2-->
+        <section> 
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Describe your development environment (physical hardware and logical software)</p></h3>
+                        <br><br>
+                        <p class ="text-intro" style="color:white;">
+                            Starting of with logical software environment, I personally use Visual Studio Code. Why I Prefer using visual code is becuase It supports hundreds of 
+                            languages, VS Code helps you be instantly productive with syntax highlighting, bracket-matching, auto-indentation, box-selection, 
+                            snippets, and more. Intuitive keyboard shortcuts, easy customization and community-contributed keyboard shortcut mappings 
+                            let you navigate your code with ease.
+                        </p>
+
+                        <p class ="text-intro" style="color:white;">
+                            For physical hardware I use Windows because its very easy to install software packages onto a windows operating system than any
+                            other operating system. Its very user friendly 
+
+
+                            </p>
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                <center><img src ="https://i.gifer.com/Dmye.gif" alt ="" style="width:500px;height:400px; border-radius:20px;"  ></center>
+            </div>
+            
+            
+            
+        </section>     
+        <section>
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+        <div class="wrap">
+            <div class="grid vertical-align">
+                
+                    
+                <div class="wrap size-50 aligncenter">
+                    <h3><p style="color:white;">Draw a diagram development environment (physical hardware and logical software).</p></h3>
+                    <br><br><br><br><br>
+                    
+                
+                    <div class="column">
+                <img src="img/diafram1.jpg"   alt="" style="border-radius:20px;">
+                
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+        
+        <!--slide 3-->
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Describe your experience with Lab 1 (client-side dynamic table of squares and cubes 1-30)</p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            My experience with Lab 1 was very informative as I Hardcoded client-side dynamic table of squares and cubes 1-30 in html. 
+                            As I gave a thought to doing it in javascript but I think Harcoding for me was fairly easy and straight forward. If it was to 
+                            square and cube numbers more than 30 than I woule've went with implementing it in javascript. 
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <!--slide 4-->
+        <section>
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+        <div class="wrap">
+            <div class="grid vertical-align">
+                
+                    
+                <div class="wrap size-50 aligncenter">
+                    <h3><p style="color:white;">Display your solution to Lab 1 and/or provide a screenshot representing any challenges you encountered.</p></h3>
+                    <br><br><br><br><br>
+                    
+                
+                    <div class="column">
+                <img src="img/lab1.jpg"   alt="" style="border-radius:20px;">
+                
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Describe your experience with Lab 2 </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            My experience with Lab 2 was a little bit challenging beacuse I was also experiencing some cache issues as i was 
+                            changing some of the attribute values in css and it was not reflecting on the localhost browser. So I figure to open the php 
+                            in incognito and I it was still having some chace issues for some reason. So I had to clear my cache and cookies for 
+                            every other refresh. 
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+        <div class="wrap">
+            <div class="grid vertical-align">
+                
+                    
+                <div class="wrap size-50 aligncenter">
+                    <h3><p style="color:white;">Display your solution to Lab 2 and/or provide a screenshot representing any challenges you encountered.</p></h3>
+                    <br><br><br><br><br>
+                    
+                
+                    <div class="column">
+                <img src="img/lab_2.jpg"   alt="" style="border-radius:20px;">
+                
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Display one or more paragraphs (less than 1,000 words) describing your proposed theme for your CSC 443 themepark pages and how you expect to use Internet Programming to express your ideas about your theme </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                            My proposed theme is about Southern connecticut State University. I thought about making a theme for SCSU beacause it is my
+                            last semester here. My Theme has valueable information about our University. Explaning Cources, how to apply, media, Contact and more.
+                            I did add some href to the main SCSU pages, So that people can locate the right information from the links and buttons.
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <section>
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Display your team members' names and a link to each of their themepark home pages </p></h3>
+                        <br><br>
+                      
+                            <br>
+                            <form action="">
+                            <button  class="button-17" role="button" type="submit" >Eric Miguel</button>
+                            </form>
+                            <form action=" http://167.99.253.247/csc443/stofkoj1/csc443projects/assignments/A3/">
+                            <button class="button-17" role="button" type="submit" >Jeffrey Stofko</button>
+                            </form>
+                            <form action="https://167.99.253.247/csc443/pollardh1/Assignment%203">
+                            <button class="button-17" role="button" type="submit">Ryan Pollard</button>
+                            </form>
+                                <form action=" ">
+                            <button class="button-17" role="button" type="submit">Brianny Perez</button>
+                                    </form>
+                            
+                       
+
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Describe how your web presentation is hosted and how you made your presentation web-accessible. </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                           My Presentation is hosted locally and on public ip address. I made my presentation web-accessible by uploading my files to github 
+                           and then git cloning my files in the shared droplet that we are using.  
+
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        
+        <section>
+            <span class="background" style="background-image:url('img/slide2.jpg')"></span>
+        <div class="wrap">
+            <div class="grid vertical-align">
+                
+                    
+                <div class="wrap size-50 aligncenter">
+                    <h3><p style="color:white;">Draw a diagram detailing how your web presentation is hosted. Include the URL of your page and the IP address of the web server.</p></h3>
+                    <br><br><br><br><br>
+                    
+                
+                    <div class="column">
+                <img src="img/host.jpg"   alt="" style="border-radius:20px;">
+                
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+        <section>
+            
+            
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+            <div class="wrap">
+                <div class="grid vertical-align">
+              <center>      <div class="text">
+                        
+                        <h3 ><p style="color:white;">Indicate at least one question would like to explore and eventually be able to answer have about any of the following topics as they relate to your plans for your theme park page(s) </p></h3>
+                        <br><br>
+                        <p class ="box" style="color:black">
+                            <br>
+                           I would like to explore on how session works in php and client side javascript.
+                           I know that a session is used to store information that can be accessed by the multiple pages of a web application, but I would like 
+                           to actually implement this in my upcoming presentations
+                            <br> <br>
+                        </p>
+                        
+                        
+                    
+                    </div></center>
+                    <br><br>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        <!--slide 12-->
+        <section>
+            <span class="background" style="background-image:url('img/slide1.jpg')"></span>
+           <center>     <div class="wrap">
+                    
+                    <h1><p style="color:white;">Thank you everyone!</p>                       
+                    </h1>
+                    
+                    <br><br>
+                    <h2 ><p style="color:white;">Jasdip Singh </p></h2>
+                    
+                    <img src="img/owl.png" alt="" style="width:500px;height:200px;"> <br><br>
+                    <input type="button" class="button" onclick="location.href='index.html#slide=1'" style="width:auto; border-radius:20px;" value="Begin">
+                </div> </center>
+                </section>
+        </section>
+        <script src="js/webslides.js"></script>  
+        <script>
+            window.ws = new WebSlides();
+        </script> 
+    </body>
+</html>
